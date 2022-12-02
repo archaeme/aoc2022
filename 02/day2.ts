59675e365cb4f3d4abcad81541ed941ebe12f6f1
Francis Dinh <normandy@biribiri.dev>
2022-12-02 16:41:27 -0500

    Improve day 2 code

--- 02/day2.ts
+++ 02/day2.ts
@@ -1,66 +1,62 @@
 import { readFile } from 'node:fs/promises';
 
-// A - Rock; B - Paper; C - Scissor
-type Opponent = 'A'|'B'|'C';
-// X - Rock; Y - Paper; Z - Scissor
-type Player = 'X'|'Y'|'Z';
-type Part1Round = [Opponent, Player];
-
-// X - Lose; Y - Draw; Z - Win
-type DesiredOutcome = 'X'|'Y'|'Z';
-type Part2Round = [Opponent, DesiredOutcome];
-type Round = Part1Round | Part2Round;
-
-enum Outcome {
+enum OutcomeScore {
 	Loss = 0,
 	Draw = 3,
 	Win = 6,
 }
 
-enum PlayerShape {
+enum ShapeScore {
 	Rock = 1,
 	Paper = 2,
 	Scissor = 3,
 }
 
+enum Shape {
+	Rock = 0,
+	Paper = 1,
+	Scissor = 2,
+}
+
+enum Outcome {
+	Loss = 0,
+	Draw = 1,
+	Win = 2,
+}
+
+type Round = [number, number];
+type Part1Round = [Shape, Shape];
+type Part2Round = [Shape, Outcome];
+
+function normalizeRound(rawRound: string): Round {
+	return [rawRound.charCodeAt(0) - 'A'.charCodeAt(0), rawRound.charCodeAt(2) - 'X'.charCodeAt(0)];
+}
+
+function shapeToScore(shape: Shape): ShapeScore {
+	switch (shape) {
+		case Shape.Rock:
+			return ShapeScore.Rock;
+		case Shape.Paper:
+			return ShapeScore.Paper;
+		case Shape.Scissor:
+			return ShapeScore.Scissor;
+	}
+}
+
 function part1(rounds: Part1Round[]): number {
-	function outcome(opponent: Opponent, player: Player): Outcome {
-		if (opponent === 'A' && player === 'X') {
-			return Outcome.Draw;
-		} else if (opponent === 'A' && player === 'Y') {
-			return Outcome.Win;
-		} else if (opponent === 'A' && player === 'Z') {
-			return Outcome.Loss;
-		} else if (opponent === 'B' && player === 'X') {
-			return Outcome.Loss;
-		} else if (opponent === 'B' && player === 'Y') {
-			return Outcome.Draw;
-		} else if (opponent === 'B' && player === 'Z') {
-			return Outcome.Win;
-		} else if (opponent === 'C' && player === 'X') {
-			return Outcome.Win;
-		} else if (opponent === 'C' && player === 'Y') {
-			return Outcome.Loss;
-		} else if (opponent === 'C' && player === 'Z') {
-			return Outcome.Draw;
+	function outcome(round: Part1Round): OutcomeScore {
+		const [opponent, player] = round;
+		if (opponent === player) {
+			return OutcomeScore.Draw;
+		} else if ((player + 1) % 3 === opponent) {
+			return OutcomeScore.Loss;
 		}
+		return OutcomeScore.Win;
 	}
 	let score = 0;
 	for (const round of rounds) {
 		const [opponent, player] = round;
-		let roundScore = 0;
-		switch (player) {
-		case 'X':
-			roundScore += PlayerShape.Rock + outcome(...round);
-			break;
-		case 'Y':
-			roundScore += PlayerShape.Paper + outcome(...round);
-			break;
-		case 'Z':
-			roundScore += PlayerShape.Scissor + outcome(...round);
-			break;
-		}
-		score += roundScore;
+		score += shapeToScore(player) + outcome(round);
 	}
 	return score;
 }
@@ -68,51 +64,18 @@ function part1(rounds: Part1Round[]): number {
 function part2(rounds: Part2Round[]): number {
 	let score = 0;
 	for (const round of rounds) {
-		const [opponent, desiredOutcome] = round;
+		const [opponent, outcome] = round;
 		let roundScore = 0;
-		switch (desiredOutcome) {
-		case 'X': // Loss
-			roundScore += Outcome.Loss;
-			switch (opponent) {
-			case 'A': 
-				roundScore += PlayerShape.Scissor;
+		switch (outcome) {
+			case Outcome.Loss:
+				roundScore += OutcomeScore.Loss + shapeToScore((((opponent - 1) % 3) + 3) % 3);
 				break;
-			case 'B': 
-				roundScore += PlayerShape.Rock;
+			case Outcome.Draw:
+				roundScore += OutcomeScore.Draw + shapeToScore(opponent);
 				break;
-			case 'C': 
-				roundScore += PlayerShape.Paper;
+			case Outcome.Win:
+				roundScore += OutcomeScore.Win + shapeToScore((opponent + 1) % 3);
 				break;
-			}
-			break;
-		case 'Y': // Draw
-			roundScore += Outcome.Draw;
-			switch (opponent) {
-			case 'A':
-				roundScore += PlayerShape.Rock;
-				break;
-			case 'B':
-				roundScore += PlayerShape.Paper;
-				break;
-			case 'C':
-				roundScore += PlayerShape.Scissor;
-				break;
-			}
-			break;
-		case 'Z': // Win
-			roundScore += Outcome.Win;
-			switch (opponent) {
-			case 'A': 
-				roundScore += PlayerShape.Paper;
-				break;
-			case 'B': 
-				roundScore += PlayerShape.Scissor;
-				break;
-			case 'C': 
-				roundScore += PlayerShape.Rock;
-				break;
-			}
-			break;
 		}
 		score += roundScore;
 	}
@@ -120,7 +83,7 @@ function part2(rounds: Part2Round[]): number {
 }
 
 const rawData = await readFile('input.txt', { encoding: 'utf8' });
-const data: Round[] = rawData.split('\n').map(s => s.split(' '));
+const data: Round[] = rawData.split('\n').filter(line => line !== '').map(normalizeRound);
 
 console.log(part1(data as Part1Round[]));
 console.log(part2(data as Part2Round[]));
