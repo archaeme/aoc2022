cb985fc26f9a6b51888b4189418f7c64073d6469
Francis Dinh <normandy@biribiri.dev>
2022-12-02 16:42:01 -0500

    day1: Remove redundant function

--- 01/day1.ts
+++ 01/day1.ts
@@ -1,14 +1,10 @@
 import { readFile } from 'node:fs/promises';
 
-function maxCalories(calories: number[]): number {
-	return Math.max(...calories);
-}
-
 function topThreeTotal(calories: number[]): number {
 	let topThree: number[] = [];
 	for (let i = 0; i < 3; i++) {
 		const remaining = calories.filter(n => !topThree.includes(n));
-		topThree.push(maxCalories(remaining));
+		topThree.push(Math.max(...remaining));
 	}
 
 	return topThree.reduce((acc, curr) => acc + curr);
